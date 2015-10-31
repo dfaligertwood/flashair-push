c5d838a04b402f55edf982bae72109e57d6b4236
Douglas Ligertwood <dfaligertwood@dunelm.org.uk>
2015-10-31 00:45:27 +0000

    First actual commit...

--- push.lua
+++ push.lua
@@ -1,5 +1,93 @@
-logFile     = "/lua/sent_files.txt"
-watchFolder = "/DCIM/101_CANON"
+logFilePath     = "/lua/sent_files.txt"
+errorFilePath   = "/lua/failed_files.txt"
+bufferFilePath  = "/lua/unsent_files.txt"
+newestFilePath  = "/lua/newest_file.txt"
+serverUrl       = "http://192.168.0.11:8080"
+watchFolder     = "/DCIM/101_CANON"
 
-print(fa.ReadStatusReg())
 
+function httpSuccess(code)
+  local firstNum = string.sub(code, 1, 1)
+  return (firstNum == '2' or firstNum == '1')
+end
+
+function sendFile(filePath)
+  local filesize = lfs.attributes(filePath, "size")
+
+  if filesize then
+    local body, code, header = fa.request { url = serverPath
+                                          , method = "PUT"
+                                          , headers = {["Content-Length"] = filesize}
+                                          , file = filePath
+                                          }
+    if httpSuccess(code) then
+      logFile:write(filePath)
+      logFile:flush()
+    else
+      errorFile:write(filePath..' Code: '..code..'\n')
+      errorFile:flush()
+    end
+  else
+    errorFile:write(filePath..' does not exist.'..'\n')
+    errorFile:flush()
+  end
+end
+
+function sendFiles(filePaths)
+  for i = 1, #filePaths do
+    sendFile(filePaths[i])
+  end
+end
+
+function checkDir()
+  local directory = lfs.dir(watchFolder)
+  local f = io.open(newestFilePath)
+  if f then
+    local newestFile = f:read()
+    f:close()
+    if newestFile then
+      local newestFileDate = lfs.attributes(newestFile, 'modification')
+    end
+  else
+    newestFileDate = 0
+  end
+
+  for file in directory do
+    local filePath = watchFolder..file
+    local fileModDate = lfs.attributes(filePath, 'modification')
+    if fileModDate > newestFileDate then
+      local f = io.open(newestFilePath, 'w+')
+      f:write(filePath)
+      f:close()
+      return filePath
+    end
+  end
+
+  return nil
+end
+
+function bufferedFiles(newFile)
+  local t = {}
+  for f in io.lines(bufferFile) do
+    table.insert(t, f)
+  end
+  table.insert(t, newFile)
+  io.open(bufferFile, 'w+'):close()
+  return t
+end
+
+local newFile = checkDir()
+if newFile then
+  local res = fa.ReadStatusReg()
+  if (string.sub(res, 13, 13) == "b") then
+    logFile   = io.open(logFilePath, 'a+')
+    errorFile = io.open(errorFilePath, 'a+')
+    sendFiles(bufferedFiles(newFile))
+    logFile:close()
+    errorFile:close()
+  else
+    local bufferFile = io.open(bufferFilePath, 'a+')
+    bufferFile:write(newFile..'\n')
+    bufferFile:close()
+  end
+end
