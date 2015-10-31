91f4d4d914f52dc9684332c26252e7d959f25968
Douglas Ligertwood <dfaligertwood@dunelm.org.uk>
2015-10-31 00:54:12 +0000

    bugfix

--- push.lua
+++ push.lua
@@ -53,7 +53,7 @@ function checkDir()
   for file in lfs.dir(watchFolder) do
     local filePath = watchFolder..file
     local fileModDate = lfs.attributes(filePath, 'modification')
-    if fileModDate > newestFileDate then
+    if fileModDate and fileModDate > newestFileDate then
       local f = io.open(newestFilePath, 'w+')
       f:write(filePath)
       f:close()
