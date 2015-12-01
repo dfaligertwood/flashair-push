a2d0f1284f6f7b691bb3d5cbe96372cccd8a13e7
Douglas Ligertwood <dfaligertwood@landroval.local>
2015-12-01 11:54:54 +0000

    First commit of server

---
+++ server/src/Main.hs
@@ -0,0 +1,64 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+--------------------------------------------------------------------------------
+
+module Main where
+
+import           Control.Concurrent.Async
+  ( race_ )
+import           Control.Concurrent.STM
+  ( atomically
+  , TChan
+  , newTChan
+  , readTChan
+  , writeTChan )
+import           Control.Monad
+  ( forever )
+import           Control.Monad.IO.Class
+  ( liftIO )
+import           Data.Aeson
+  ( FromJSON )
+import qualified Data.ByteString.Lazy as B
+import           Data.ByteString.Char8
+  ( ByteString
+  , unpack )
+import           GHC.Generics
+  ( Generic )
+import           Network.HTTP.Conduit
+  ( simpleHttp )
+import           Snap.Core
+import           Snap.Http.Server
+import           Snap.Extras.JSON
+import           System.FilePath
+  ( takeFileName )
+
+--------------------------------------------------------------------------------
+
+data File = File { file :: String } deriving (Generic)
+instance FromJSON File where
+
+--------------------------------------------------------------------------------
+
+main :: IO ()
+main = do
+    downloadQueue <- atomically newTChan
+    race_ (download downloadQueue)
+          (quickHttpServe $ site downloadQueue)
+
+site :: TChan (ByteString, File) -> Snap ()
+site q = ifTop . method POST $ do
+    payload <- reqJSON
+    remote <- getsRequest rqRemoteAddr
+    _ <- liftIO . atomically $ writeTChan q (remote, payload)
+    return ()
+
+download :: TChan (ByteString, File) -> IO ()
+download q = forever $ do
+    (u, f) <- atomically $ do
+        (ip, f) <- readTChan q
+        let fileName = file f
+        let url = "http://" ++ unpack ip ++ fileName
+        return (url, fileName)
+    putStrLn $ "Downloading " ++ f
+    simpleHttp u >>= B.writeFile (takeFileName f)
+    putStrLn $ "Finished " ++ f
